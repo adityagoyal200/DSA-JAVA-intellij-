84c9dfcca826e01f766044902315b488e4443597
Aditya goyal <goyaladitya3815@gmail.com>
2022-11-22 02:01:50 +0530

    searching charcter in string

---
+++ SearchInString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SearchInString {
+    public static void main(String[] args) {
+        String name="aditya";
+        char target='a';
+        System.out.println(search(name,target));
+    }
+    static boolean search(String arr, char target){
+        if(arr.length()==0) {
+            return false;
+        }
+            for(int i=0;i<arr.length();i++){
+                if(target==arr.charAt(i)){
+                    return true;
+                }
+            }
+        return false;
+    }
+}
