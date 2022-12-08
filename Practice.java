ed535428f787ae217297e466dab331807b17fa04
Aditya goyal <goyaladitya3815@gmail.com>
2022-12-08 19:47:38 +0530

    searching in 2d array

---
+++ Practice.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+    public class Practice{
+        public static void main(String[] args) {
+            int[] prac = new int[10];
+            Scanner obj = new Scanner(System.in);
+            System.out.println("enter the array: ");
+            for (int i = 0; i < prac.length; i++) {
+                prac[i]=obj.nextInt();
+            }
+            System.out.println("the array entered is: ");
+            for(int i=0;i<prac.length;i++){
+                System.out.print(prac[i] + " ");
+            }
+            System.out.println(search(prac,4));
+        }
+//        static  int linearSearch(int[] arr, int target){
+//            if(arr.length==0){
+//                return -1;
+//            }
+//            for(int i=0;i<arr.length;i++){
+//                if(arr[i]==target){
+//                    return i;
+//                }
+//            }
+//            System.out.println("not found");
+//            return -1;
+//        }
+        static int search(int[] array,int target){
+            if(array.length==0){
+                return -1;
+            }
+            for(int i=0;i<array.length;i++) {
+                if (array[i] == target) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+
+
+
